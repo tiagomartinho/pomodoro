789bbbc6f4511f99c0ad23a99595799418a7eefa
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-06-30 20:43:03 +0200

    refactor tracker class

--- Pomodoro/Tracker.swift
+++ Pomodoro/Tracker.swift
@@ -1,53 +1,48 @@
 import Foundation
 
 class Tracker{
-
-    private var timeInterval:NSTimeInterval{
-        if let backgroundDate = defaults.objectForKey(kUD_StartDate) as? NSDate {
-            return abs(backgroundDate.timeIntervalSinceNow)
-        }
-        else{
-            return 0
-        }
-    }
     
     var isRunning:Bool{
         return defaults.objectForKey(kUD_isTimerRunning) as? Bool ?? false
     }
     
     var description:IntervalType {
-        return Intervals().intervalType(timeInterval)
+        return Intervals().intervalType(self.timeInterval)
     }
     
     var time:String{
-        let intervalDurationInSeconds = Double(Intervals().intervalDurationInSeconds(timeInterval))
-        let previousIntervals = Double(Intervals().cumulativeIntervalDurationInSeconds(timeInterval)) - intervalDurationInSeconds
-        let timePassed = timeInterval-previousIntervals
-        let timeLeft = Int(intervalDurationInSeconds - timePassed)
+        let timeLeft = Int(self.timeLeft)
         if timeLeft >= 0 {
-            let minutesLeft = timeLeft/60
-            let secondsLeft = timeLeft%60
-            return String.formatTimer(minutesLeft, seconds: secondsLeft)
+            return timeFormatted(timeLeft)
         }
-        else {
-            return "25:00"
-        }
+        return "25:00"
+    }
+    
+    private func timeFormatted(timeInSeconds:Int)->String{
+        let minutesLeft = timeInSeconds/60
+        let secondsLeft = timeInSeconds%60
+        return String.formatTimer(minutesLeft, seconds: secondsLeft)
     }
     
     var progress:Double{
         if timeLeft >= 0 && currentIntervalDuration > 0{
             return (timeLeft/currentIntervalDuration) * 100
         }
-        else {
-            return 100.0
-        }
+        return 100.0
     }
     
-    var currentIntervalDuration:Double{
+    private var timeInterval:NSTimeInterval{
+        if let backgroundDate = defaults.objectForKey(kUD_StartDate) as? NSDate {
+            return abs(backgroundDate.timeIntervalSinceNow)
+        }
+        return 0
+    }
+    
+    private var currentIntervalDuration:Double{
         return Double(Intervals().intervalDurationInSeconds(timeInterval))
     }
     
-    var timeLeft:Double {
+    private var timeLeft:Double {
         let previousIntervals = Double(Intervals().cumulativeIntervalDurationInSeconds(timeInterval)) - currentIntervalDuration
         let timePassed = timeInterval-previousIntervals
         return currentIntervalDuration - timePassed
