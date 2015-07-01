7fae9d1237c7506c4665c5c3ff50bfc4c4d60f0b
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-01 23:21:46 +0200

    add space to long pause notification message

--- Pomodoro/Intervals.swift
+++ Pomodoro/Intervals.swift
@@ -4,7 +4,7 @@ class Intervals{
     
     static let pomodoro = Interval(type:.Pomodoro,duration: 25,message:"work".localized,cumulativeDuration:0)
     static let pause = Interval(type:.Pause,duration: 5,message:"pause".localized,cumulativeDuration:0)
-    static let longPause = Interval(type:.LongPause,duration: 15,message:"long".localized + "pause".localized,cumulativeDuration:0)
+    static let longPause = Interval(type:.LongPause,duration: 15,message:"long".localized + " " + "pause".localized,cumulativeDuration:0)
     
     let reference = [pomodoro,pause,pomodoro,pause,pomodoro,pause,pomodoro,longPause]
     private var intervals=[Interval]()
