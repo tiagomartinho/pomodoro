adff471109a437f125493dd8a497c56b93e3f527
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-01 19:40:20 +0200

    fix notification message to be the next interval instead of the current one

--- Pomodoro/Scheduler.swift
+++ Pomodoro/Scheduler.swift
@@ -11,16 +11,20 @@ class Scheduler {
     
     func schedulePomodoriAndPauses(){
         var date = 0
+        var i = 0
         for interval in Intervals().reference {
-            date += interval.duration * 60
-            scheduleNotificationWithAlertBody(interval.message,AndFireDate: NSDate(timeIntervalSinceNow: NSTimeInterval(date)))
+            date += interval.duration
+            if i+1 < count(Intervals().reference) {
+                let nextIntervalMessage = Intervals().reference[i+1].message
+                scheduleNotificationWithAlertBody(nextIntervalMessage,AndFireDate: NSDate(timeIntervalSinceNow: NSTimeInterval(date)))
+            }
+            i++
         }
     }
     
     private func scheduleNotificationWithAlertBody(alertBody:String,AndFireDate fireDate:NSDate){
         var notification:UILocalNotification = UILocalNotification()
         notification.alertBody = alertBody
-        notification.userInfo = ["userInfoKey":"userInfoValue"]
         notification.fireDate = fireDate
         notification.soundName = UILocalNotificationDefaultSoundName
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
