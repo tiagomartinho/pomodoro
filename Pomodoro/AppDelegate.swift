56586bf51d8ef6d2e369b7222e5ded1bef960cb0
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-02 22:02:16 +0200

    introduced crashlytics

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics()])
         return true
     }
 }
